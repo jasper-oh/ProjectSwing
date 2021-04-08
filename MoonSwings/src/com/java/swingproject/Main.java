4b69124c80f7629986c65a2d00d43ba150f03e36
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-08 11:51:33 +0900

    Day 9

--- MoonSwings/src/com/java/swingproject/Main.java
+++ MoonSwings/src/com/java/swingproject/Main.java
@@ -4,11 +4,22 @@ import java.awt.Color;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Main {
 
 	public static void main(String[] args) {
 	 
+		
+		
+		
+		//JLabel = a GUI display area for a string of text, an image, or both
+		JLabel label = new JLabel(); // create a label
+		label.setText("작팀삼분"); // set text of label				
+		
+		// JFrame
 		MyFrame myFrame = new MyFrame();
+		myFrame.add(label);
+		
 	}
 }
