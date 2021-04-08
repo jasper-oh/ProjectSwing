c808d0a0468d3c43a6c0978fa5cae1336c2897b4
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-08 11:14:19 +0900

    Frame class inheritance

--- MoonSwings/src/com/java/swingproject/Main.java
+++ MoonSwings/src/com/java/swingproject/Main.java
@@ -8,17 +8,7 @@ import javax.swing.JFrame;
 public class Main {
 
 	public static void main(String[] args) {
-	 JFrame frame = new JFrame();
-	 frame.setTitle("작심삼분");
-	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	 frame.setResizable(true);
-	 frame.setSize(1400,800);
-	 frame.setVisible(true);
-
 	 
-	 ImageIcon image = new ImageIcon("teammaker.png"); //create an ImageIcon
-//	 frame.setIconImage(image.getImage()); //change icon of frame // IguessNotWorkAtMac Doesn't have iconFrame
-	 frame.getContentPane().setBackground(new Color(2,130,255)); //change color of background
-	 
+		MyFrame myFrame = new MyFrame();
 	}
 }
--- MoonSwings/src/com/java/swingproject/MyFrame.java
+++ MoonSwings/src/com/java/swingproject/MyFrame.java
@@ -1,7 +1,22 @@
 package com.java.swingproject;
 
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class MyFrame extends JFrame {
+	MyFrame(){
+		//	JFrame this = new JFrame(); //  'Cause of inheritance we do not need to call instance
+		 this.setTitle("작심삼분");
+		 this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 this.setResizable(true);
+		 this.setSize(1400,800);
+		 this.setVisible(true);
 	
+		 
+//		 ImageIcon image = new ImageIcon("teammaker.png"); //create an ImageIcon
+	//	 this.setIconImage(image.getImage()); //change icon of this // IguessNotWorkAtMac Doesn't have iconthis
+		 this.getContentPane().setBackground(new Color(2,130,255)); //change color of background
+	}
 }
