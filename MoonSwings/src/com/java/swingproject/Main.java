332bf9878474d6ec97267a230b236c7f503a72bd
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-08 10:30:22 +0900

    First setting

---
+++ MoonSwings/src/com/java/swingproject/Main.java
@@ -0,0 +1,23 @@
+package com.java.swingproject;
+
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+	 JFrame frame = new JFrame();
+	 frame.setTitle("작심삼분");
+	 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 frame.setResizable(true);
+	 frame.setSize(1400,800);
+	 frame.setVisible(true);
+
+	 
+	 ImageIcon image = new ImageIcon("teammaker.png"); //create an ImageIcon
+	 frame.setIconImage(image.getImage()); //change icon of frame
+	 frame.getContentPane().setBackground(new Color(2,130,255)); //change color of background
+	}
+}
