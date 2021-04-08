f636cf15ca0a1b29e4ea24aa64b8b26396b1b4dc
jasper <63331153+jasper-oh@users.noreply.github.com>
2021-04-08 11:53:23 +0900

    image & text label

--- MoonSwings/src/com/java/swingproject/Main.java
+++ MoonSwings/src/com/java/swingproject/Main.java
@@ -14,8 +14,11 @@ public class Main {
 		
 		
 		//JLabel = a GUI display area for a string of text, an image, or both
+		ImageIcon image = new ImageIcon("teammaker.png");
+		
 		JLabel label = new JLabel(); // create a label
-		label.setText("작팀삼분"); // set text of label				
+		label.setText("작팀삼분"); // set text of label	
+		label.setIcon(image);
 		
 		// JFrame
 		MyFrame myFrame = new MyFrame();
